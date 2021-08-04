f279825e1fe5adc4c86abd4fb788f12b7b0a507d
leonardototti <leonardo.totti@edu.unifil.br>
2021-08-04 12:41:57 -0400

    fixed and customized nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ import '../styles/normalize.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import NProgress from 'nprogress'
+import "nprogress/nprogress.css"
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
